4ddf44d832592310b0cc03d07783c332a7772bec
kaelima <jeppo_meyer@msn.com>
2011-05-05 11:57:55 +0200

    Banish and Cyclone should recieve hots/dots (though they are immune ofc) - fixed :p. Also fix if you cast banish on a banished target it should remove the banish effect and not refresh it.

---
+++ sql/private/world/6_world_spell_script_names.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (710,18647);
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(710, 'spell_warl_banish'),
+(18647, 'spell_warl_banish');
