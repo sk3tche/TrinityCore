e376872d7df446133164ef019108813c9bf42730
kaelima <jeppo_meyer@msn.com>
2011-05-05 12:33:46 +0200

    Fix crit by player totems/pets

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -10661,7 +10661,9 @@ int32 Unit::SpellBaseDamageBonusForVictim(SpellSchoolMask schoolMask, Unit *pVic
 bool Unit::isSpellCrit(Unit *pVictim, SpellEntry const *spellProto, SpellSchoolMask schoolMask, WeaponAttackType attackType) const
 {
     // Mobs can't crit with spells.
-    if (IS_CREATURE_GUID(GetGUID()))
+    // However player pets/totems can.
+    if (IS_CREATURE_GUID(GetGUID()) &&
+        !(GetOwner() && GetOwner()->GetTypeId() == TYPEID_PLAYER))
         return false;
 
     // not critting spell
