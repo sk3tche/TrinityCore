a069a067b22334a37071f1346c7d29d09948ef9a
kaelima <jeppo_meyer@msn.com>
2011-05-05 12:09:41 +0200

    Fix coefficient and dispel mechanic of divine hymn/hymn of hope

---
+++ sql/private/world/7_world_spell_bonus_data.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_bonus_data` WHERE `entry`=64844;
+INSERT INTO `spell_bonus_data` (`entry`,`direct_bonus`,`dot_bonus`,`ap_bonus`,`ap_dot_bonus`,`comments`) VALUES
+(64844,0.3762,-1,-1,-1, 'Priest - Divine Hymn'); 
