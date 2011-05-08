58c06a84764349be4326be41ecb3ebd233b96a0c
Brennan Kastner <sk3tche@gmail.com>
2011-05-08 18:31:06 -0500

    Fix compile that kaelima screwed up when working with magnets. Way to go bro, way to go......

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -2056,16 +2056,20 @@ void Spell::SelectEffectTargets(uint32 i, uint32 cur)
             switch(cur)
             {
                 case TARGET_UNIT_TARGET_ENEMY:
-                    if (Unit* magnet = m_caster->SelectMagnetTarget(target, m_spellInfo) && m_spellInfo->AttributesEx2 & ~SPELL_ATTR2_CANT_HIT_MAGNET)
+                    Unit* magnet = m_caster->SelectMagnetTarget(target, m_spellInfo);
+                    if (magnet && m_spellInfo->AttributesEx2 & ~SPELL_ATTR2_CANT_HIT_MAGNET)
                         if (magnet != target)
                             m_targets.setUnitTarget(magnet);
                     pushType = PUSH_CHAIN;
                     break;
                 case TARGET_UNIT_TARGET_ANY:
                     if (!IsPositiveSpell(m_spellInfo->Id))
-                        if (Unit* magnet = m_caster->SelectMagnetTarget(target, m_spellInfo) && m_spellInfo->AttributesEx2 & ~SPELL_ATTR2_CANT_HIT_MAGNET)
+                    {
+                        Unit* magnet = m_caster->SelectMagnetTarget(target, m_spellInfo);
+                        if (magnet && m_spellInfo->AttributesEx2 & ~SPELL_ATTR2_CANT_HIT_MAGNET)
                             if (magnet != target)
                                 m_targets.setUnitTarget(magnet);
+                    }
                     pushType = PUSH_CHAIN;
                     break;
                 case TARGET_UNIT_CHAINHEAL:
